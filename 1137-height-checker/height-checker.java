2a31cb37906e4e4e97ecc6640a30dac593ce7a08
Praneetha Siddireddy <siddireddysaipraneetha@gmail.com>
2026-09-20 21:01:52 +0530

    Time: 5 ms (33.38%) | Memory: 43.6 MB (13.38%) - LeetSync

--- 1137-height-checker/height-checker.java
+++ 1137-height-checker/height-checker.java
@@ -1,10 +1,11 @@
-class Solution {
-    public int heightChecker(int[] heights) {
-        int[] expected = heights.clone();
+class Solution{
+    public int heightChecker(int[] heights) 
+    {
+        int[] expected=heights.clone();
         java.util.Arrays.sort(expected);
-        int count = 0;
-        for (int i = 0; i < heights.length; i++) {
-            if (heights[i] != expected[i]) {
+        int count=0;
+        for(int i=0;i<heights.length;i++){
+            if(heights[i]!=expected[i]){
                 count++;
             }
         }
